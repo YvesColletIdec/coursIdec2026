054f0cb1f7057cb600157b3a756254492cb731ea
YvesColletIdec <yves.collet.idec@gmail.com>
2026-01-12 18:40:39 +0100

    premier commit

---
+++ MonDeuxiemeProjet/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MonDeuxiemeProjet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*demander 2 nombres à un utilisateur
+            et afficher la somme des 2 nombres*/
+            int num1 = 0, num2 = 0, resultat = 0;
+            Console.WriteLine("saisir le num1");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("saisir le num2");
+            num2 = int.Parse(Console.ReadLine());
+            resultat = num1 + num2;
+            Console.WriteLine($"la somme de {num1} et {num2} fait {resultat}");
+        }
+    }
+}
---
+++ MonPremierProjet/Program.cs
@@ -0,0 +1,7 @@
+﻿//VAR
+string texte = "Hello, World!";
+int age = 23;
+age = int.Parse("24");
+Console.WriteLine("Saisis ton prénom");
+string prenom = Console.ReadLine();
+Console.WriteLine($"Salut {prenom} comment ça va?");
