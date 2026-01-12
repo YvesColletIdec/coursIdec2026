a94c80cc5cb050b44b705208d177395544afc1c6
Normand <normand.aymon@outlook.com>
2026-01-12 18:50:24 +0100

    test comment

--- MonDeuxiemeProjet/Program.cs
+++ MonDeuxiemeProjet/Program.cs
@@ -7,7 +7,8 @@
             /*demander 2 nombres à un utilisateur
             et afficher la somme des 2 nombres*/
             int num1 = 0, num2 = 0, resultat = 0;
-            Console.WriteLine("saisir le tamère");
+            Console.WriteLine("saisir le num1");
+            //comment
             num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("saisir le num2");
             num2 = int.Parse(Console.ReadLine());
