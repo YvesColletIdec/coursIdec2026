194d46754f3db317a3a78c315b35537a35bcc17c
Normand <normand.aymon@outlook.com>
2026-01-12 18:49:16 +0100

    j'ai changé un truc

--- MonDeuxiemeProjet/Program.cs
+++ MonDeuxiemeProjet/Program.cs
@@ -7,7 +7,7 @@
             /*demander 2 nombres à un utilisateur
             et afficher la somme des 2 nombres*/
             int num1 = 0, num2 = 0, resultat = 0;
-            Console.WriteLine("saisir le num1");
+            Console.WriteLine("saisir le tamère");
             num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("saisir le num2");
             num2 = int.Parse(Console.ReadLine());
