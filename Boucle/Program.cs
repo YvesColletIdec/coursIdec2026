bda9aaa466739861d15dc32c972f8e2aeafde11a
YvesColletIdec <yves.collet.idec@gmail.com>
2026-01-12 21:10:44 +0100

    2ème cours c#

---
+++ Boucle/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Boucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int numero = 2;
+            ////tant que
+            //while (numero == 2)
+            //{
+            //    Console.WriteLine(DateTime.Now);
+            //    //if (DateTime.Now.ToString("HH:mm:ss") == "20:26:03")
+            //    break;
+            //}
+
+            //int numero = 0;
+            //while (numero <= 0)
+            //{
+            //    Console.WriteLine("saisi un numero plus grand que 0");
+            //    numero = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine($"youpie tu as saisi {numero}");
+
+
+            //int numero = 4;
+            //do
+            //{
+            //    Console.WriteLine("saisi un numero plus grand que 0");
+            //    numero = int.Parse(Console.ReadLine());
+            //} while (numero <= 0);
+
+
+            //int j = 1;
+            //for (; ; )
+            //{
+            //    Console.WriteLine(++j);
+            //    --j;
+            //    j--;
+            //    if (j > 10)
+            //    {
+            //        break;
+            //    }
+            //}
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int numero = 3;
+            if (numero > 0 || numero < 5)
+            {
+                Console.WriteLine(">0 et <5");
+            } else
+            {
+                Console.WriteLine("faux");
+            }
+        }
+    }
+}
---
+++ Conditions/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Conditions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialisation de la variable
+            int numero = 0;
+            Console.WriteLine("saisir un chiffre plus grand que 0");
+            numero = int.Parse(Console.ReadLine());
+
+            //selon que
+            switch (numero)
+            {
+                case 2: Console.WriteLine(2);
+                    break;
+                case 3: Console.WriteLine(3);
+                    break;
+                default: Console.WriteLine("autre");
+                    break;
+            }
+
+            if (numero <= 0)
+            {
+                Console.WriteLine("Saisie eronnée");
+            }
+            else
+            {
+                Console.WriteLine("Saisie correcte");
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("pair");
+                }
+                else
+                {
+                    Console.WriteLine("impair");
+                }
+            }
+            //if simplifié
+            if (numero == 3)
+                Console.WriteLine("youpie");
+            Console.WriteLine("salut");
+            
+            
+        }
+    }
+}
